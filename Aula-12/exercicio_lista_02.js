dc81f146ec9ce9fcc327a6078bd45b84248f54f6
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-11-01 07:48:06 -0300

    Update exercicio_lista_02.js
    Atualização 01/11/2023 07:47

--- Aula-12/exercicio_lista_02.js
+++ Aula-12/exercicio_lista_02.js
@@ -1,14 +1,3 @@
-/* Questão 2
-
-Faça um programa que calcule a média de n números informados pelo usuário até ele informar o número 0. Por exemplo, se o usuário informar os números 9 6 2 8 0, o programa deve calcular a média (9+ 6 + 2 + 8) / 4.
-Exemplo de execução
-
-Digite um número (0 para sair): 5
-Digite um número (0 para sair): 8
-Digite um número (0 para sair): 3
-Digite um número (0 para sair): 0
-A média dos números digitados é: 5.333333333333333 */
-
 const prompt = require('prompt-sync')();
 
 let number = 1;
